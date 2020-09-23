56bf0d9beec476fb734de52b268d94431a4bcd19
dimato <dimato@users.noreply.github.com>
2020-09-23 10:00:04 +0700

    small change from last time

--- app/sublink.py
+++ app/sublink.py
@@ -7,3 +7,19 @@ from flask_mysqldb import MySQL
 def kakryan():
     return render_template('public/kakryan.html')
 
+@app.route('/atrial_fibrilation', methods=["POST"])
+def atrial_fibrilation():
+    if request.method == "POST":
+        if request.files:
+            file_diterima = request.files['atr']
+            if file_diterima.filename == "":
+                feedback = "No filename"
+                return render_template('public/atrial_fibrilation', feedback=feedback)
+            
+            filename = secure_filename(file_diterima.filename)
+            session['file_diterima'] = filename
+
+            file_diterima.save(os.path.join(app.config['IMAGE_UPLOADS'], session.get('file_diterima')))
+
+            saved_file = app.config["IMAGE_UPLOADS"] + "/" + session.get('file_diterima')
+    return render_template('public/atrial_fibrilation')
