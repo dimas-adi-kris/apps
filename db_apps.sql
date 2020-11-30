6ab3578b59411e5fd8912659b6f8f8129d493dee
dimas-adi-kris <62425346+dimas-adi-kris@users.noreply.github.com>
2020-12-01 02:08:40 +0700

    Update db_apps.sql

--- db_apps.sql
+++ db_apps.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.7.4
+-- version 5.0.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 10, 2020 at 10:33 AM
--- Server version: 10.1.29-MariaDB
--- PHP Version: 7.2.0
+-- Generation Time: Nov 30, 2020 at 08:08 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,44 +24,71 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tb_af`
+--
+
+CREATE TABLE `tb_af` (
+  `id` int(11) NOT NULL,
+  `file` varchar(128) NOT NULL,
+  `status_detect` varchar(128) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `tb_af`
+--
+
+INSERT INTO `tb_af` (`id`, `file`, `status_detect`) VALUES
+(1, 'test1-29112020-185509', 'Normal'),
+(2, 'A00004-29112020-210436', 'Atrial Fibrilation'),
+(3, 'A00001-30112020-165950', 'Normal'),
+(4, 'A00001-30112020-175925', 'Normal'),
+(5, 'A00001-30112020-180321', 'Normal'),
+(6, 'test1-30112020-210933', 'Normal'),
+(7, 'A00001-30112020-213625', 'Normal');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tb_arrdetect`
 --
 
 CREATE TABLE `tb_arrdetect` (
-  `id_detect` int(11) NOT NULL,
-  `file` varchar(255) NOT NULL,
+  `id` int(11) NOT NULL,
+  `file` text NOT NULL,
   `status_detect` enum('0','1') NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `tb_arrdetect`
---
-
-INSERT INTO `tb_arrdetect` (`id_detect`, `file`, `status_detect`) VALUES
-(1, '123', '1'),
-(2, '1111', '1'),
-(3, '1233', '0'),
-(4, 'WhatsApp_Image_2020-09-08_at_13.55.49.jpeg', '0');
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `tb_af`
+--
+ALTER TABLE `tb_af`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indexes for table `tb_arrdetect`
 --
 ALTER TABLE `tb_arrdetect`
-  ADD PRIMARY KEY (`id_detect`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
+-- AUTO_INCREMENT for table `tb_af`
+--
+ALTER TABLE `tb_af`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT for table `tb_arrdetect`
 --
 ALTER TABLE `tb_arrdetect`
-  MODIFY `id_detect` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
